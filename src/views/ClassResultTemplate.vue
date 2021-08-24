571532aca4dd72518c4caae30cbc72025f645ead
kkaleuleuleu <0127sw@naver.com>
2021-08-24 19:14:02 +0900

    -.-

---
+++ src/views/ClassResultTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex-column pageDiv">
+    <div class="subname">
+      <div class="shareButton">
+        <img src="../assets/share_icon.svg" alt="share" @click="shareToPage"/>
+      </div>
+      <slot name="subname"/>
+    </div>
+    <div class="classname">
+      <slot name="classname"/>
+    </div>
+    <div class ="classimage">
+      <slot name="classimage"/>
+    </div>
+    <div class="classintro">
+      <slot name="classintro"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
